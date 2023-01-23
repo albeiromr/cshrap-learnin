8c0fb21432046fb9c5c554ca1c79162b10488ffe
Albeiro Marin <xlandru@hotmail.com>
2023-01-23 16:39:54 -0500

    Refactoriza el resto del código

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using aplicacion1.data_types;
+using aplicacion1.variables;
 
 namespace aplicacion1
 {
@@ -12,37 +13,10 @@ namespace aplicacion1
 
             Doubles.PrintDoubles();
 
-            #region concatenaciones de strings
+            Strings.PrintStrings();
 
-            byte años = 18;
-            string meazure = " edad";
-
-            //concatenación con el simbolo mas
-            Console.WriteLine("tienes " + años + " de" + meazure);
-
-            //concatenación con interpolación
-            Console.WriteLine($"Tienes una edad de {años} años");
-
-            #endregion
-
-            #region declaración de variables
-
-            //declaración con tipo de datos--------------------------------------------
-
-
-            int edad = 30;
-            byte fingers; //al declarar con el tipo de dato se puede inicializar después
-            fingers = 5; //inicializando la variable
-            Console.WriteLine($"edad: {edad}, finguers {fingers}");
-
-            //declaración con inferencia de datos---------------------------------------
-
-
-            var name = "Andru"; //las variables con var no se pueden inicializar vacías
-                                //y quedan fuertemente tipadas, asignar un tipo de datro distinto
-            Console.WriteLine(name);
-
-            #endregion
+            Variables.PrintVariables();
+            
         }
     }
 }
---
+++ variables/Variables.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aplicacion1.variables
+{
+    internal class Variables
+    {
+        public static void PrintVariables()
+        {
+            #region declaración de variables
+
+            //declaración con tipo de datos--------------------------------------------
+
+
+            int edad = 30;
+            byte fingers; //al declarar con el tipo de dato se puede inicializar después
+            fingers = 5; //inicializando la variable
+            Console.WriteLine($"edad: {edad}, finguers {fingers}");
+
+            //declaración con inferencia de datos---------------------------------------
+
+
+            var name = "Andru"; //las variables con var no se pueden inicializar vacías
+                                //y quedan fuertemente tipadas, asignar un tipo de datro distinto
+            Console.WriteLine(name);
+
+            #endregion
+        }
+    }
+}
