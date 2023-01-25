83b1283d72231e87c1cca7697e199dae0abcb855
Albeiro Marin <xlandru@hotmail.com>
2023-01-25 08:45:24 -0500

    Agrega código de ejemplo de constantes

--- constants/Constants.cs
+++ constants/Constants.cs
@@ -10,6 +10,11 @@ namespace aplicacion1.constants
     {
         public static void PrintConstants()
         {
+            //las constantes se declaran con la palabra cont seguido del tipo de dato
+            //solo se pueden contruir constantes con los tipos de datos primitivos,
+            //no se pueden construir con tipos de datos personalizados
+            const int a = 42;
+            Console.WriteLine(a);
 
         }
     }
